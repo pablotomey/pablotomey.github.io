c270fac13ef1b435ea3caa844b412357eeaef452
PabloTP87 <pablo.tp87@gmail.com>
2020-09-07 21:24:26 -0300

    Agregado diseño definitivo y responsive

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const secciones = new fullpage('#fullpage', {
-	//options here
-	autoScrolling:true,
-	scrollHorizontally: true,
-	responsiveWidth: 1100
-});
-
-//métodos
-fullpage_api.setAllowScrolling(true);
